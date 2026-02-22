9397d489fddd01e957ac98ba3ea7bc2c8c07b2e9
mustaine611 <arboleda.paulo2001@gmail.com>
2026-02-22 16:04:57 -0500

    integracion 1 de i18n

--- src/main/java/org/example/springsecurity/controller/CustomerController.java
+++ src/main/java/org/example/springsecurity/controller/CustomerController.java
@@ -1,28 +1,35 @@
 package org.example.springsecurity.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class CustomerController {
 
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("/public")
-    public String publicEndpoint() {
-        return "Endpoint público - No requiere autenticación";
+    public String publicEndpoint(Locale locale) {
+        return messageSource.getMessage("public.endpoint", null, locale);
     }
 
     @GetMapping("/admin")
-    public String adminEndpoint() {
-        return "Bienvenido ADMIN 👑";
+    public String adminEndpoint(Locale locale) {
+        return messageSource.getMessage("admin.welcome", null, locale);
     }
 
     @GetMapping("/user")
-    public String userEndpoint() {
-        return "Bienvenido USER 👤 o ADMIN";
+    public String userEndpoint(Locale locale) {
+        return messageSource.getMessage("user.welcome", null, locale);
     }
 
     @GetMapping("/")
-    public String home() {
-        return "Home - Requiere autenticación";
+    public String home(Locale locale) {
+        return messageSource.getMessage("home.text", null, locale);
     }
 }
